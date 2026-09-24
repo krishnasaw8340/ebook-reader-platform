5d2e49cfdcb9ced0926656b0043e6f9917e07da5
Krisha Kr Saw <krishnakrsaw8340@gmail.com>
2026-09-24 21:25:26 +0530

    updated-dev-mode

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </AuthProvider>
-  </StrictMode>,
+  <AuthProvider>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </AuthProvider>
 )
